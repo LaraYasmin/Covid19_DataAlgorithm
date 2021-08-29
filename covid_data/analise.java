7b5433e05d069cd7615580d29596ac1e5293f935
Lara <lyasminoliveira18@gmail.com>
2021-08-29 15:44:07 -0300

    Leitura do arquivo csv

--- covid_data/analise.java
+++ covid_data/analise.java
@@ -1,5 +1,25 @@
 package covid_data;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class analise {
-    
+
+    public static void main(String[] args) {
+ 
+		String path = "./database/covid19-49bdf44a58bf4ebfa5415b1eabadd3db.csv"; 
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+
+		} 
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
 }
